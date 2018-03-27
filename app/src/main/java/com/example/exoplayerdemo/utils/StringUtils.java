c8f40c1202fd116fb7390b5e337b2cc1792e561f
kb18519142009 <847584597@qq.com>
2018-03-27 18:16:54 +0800

    修改工具类

--- app/src/main/java/com/example/exoplayerdemo/utils/StringUtils.java
+++ app/src/main/java/com/example/exoplayerdemo/utils/StringUtils.java
@@ -9,7 +9,7 @@ public class StringUtils {
 
         int seconds = totalSeconds % 60;
         int minutes = (totalSeconds / 60) % 60;
-        int hours   = totalSeconds / 3600;
+        int hours = totalSeconds / 3600;
 
         if (hours > 0) {
             return String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, seconds);
